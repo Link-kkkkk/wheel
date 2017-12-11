557c3f3e49232cddc530b1404a8727dbdc9d039b
Link <13788485679@sina.com>
2017-12-11 15:08:36 +0800

    vue-cli接入

--- vue-cli/src/components/remote.vue
+++ vue-cli/src/components/remote.vue
@@ -3,16 +3,26 @@
 </template>
 <script>
 export default {
+  mounted() {
+    this.$nextTick(function() {
+      // var script = document.createElement("script");
+      // script.type = "text/javascript";
+      // script.src = "http://api.map.baidu.com/api?v=2.0&ak=您的密钥&callback=init";
+      // document.body.appendChild(script);
+    });
+  },
   components: {
-    'remote': {
+    remote: {
       render(createElement) {
-        return createElement('script', { attrs: { type: 'text/javascript', src: this.src } });
+        return createElement("script", {
+          attrs: { type: "text/javascript", src: this.src }
+        });
       },
       props: {
-        src: { type: String, required: true },
-      },
-    },
-  },
-}
+        src: { type: String, required: true }
+      }
+    }
+  }
+};
 </script>
 
